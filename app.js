b582339edd298eabfcadc3446472b23e30ce8c15
Jason Ritter <jason_ritter@outlook.com>
2025-06-07 14:45:54 -0500

    Make humanChoice case insensitive

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
+let humanScore, computerScore = 0;
 
 function getComputerChoice() {
       // Get random number (separate function?)
     const randNum = Math.floor(Math.random() * 3) + 1
 
     if (randNum === 1) {
-      return "Rock";
+      return "rock";
     } else if (randNum === 2) { 
-      return "Paper";
+      return "paper";
     } else {
-      return "Scissors";
+      return "scissors";
     }
 }
 
@@ -17,8 +18,30 @@ function getHumanChoice() {
   let humanChoice = prompt("Please enter your choice");
   return humanChoice;
 }
+
+function playRound(humanChoice, computerChoice) {
+  // Convert humanChoice to all lowercase
+  console.log(humanChoice.toLowerCase());
+
+  // if humanChoice === "rock" && computerChoice === "paper"
+    // Display you lose
+
+  // else if humanChoice === "paper" && computerChoice === " scissors"
+    // Display you lose
+
+  // else if humanChoice === "scissors" && computerChoice === "rock"
+   // Display you lose
+
+  // else if humanChoice === computerChoice
+    // Display you tie
   
-console.log(getComputerChoice());
-console.log(getHumanChoice());
+  // else
+   // Display you win
+}
+
+const computerChoice = getComputerChoice();
+const humanChoice = getHumanChoice();
+  
+playRound(humanChoice, computerChoice);
 
 // End getComputerChoice function
