b92106944bd7fbd0d2c8d3b855acfa4e8b39184f
Jason Ritter <jason_ritter@outlook.com>
2025-06-07 14:16:16 -0500

    Create function getComputerChoice

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
+
+function getComputerChoice() {
+      // Get random number (separate function?)
+    const randNum = Math.floor(Math.random() * 3) + 1
+
+    if (randNum === 1) {
+      return "Rock";
+    } else if (randNum === 2) { 
+      return "Paper";
+    } else {
+      return "Scissors";
+    }
+}
+
+function getHumanChoice() {
+  // Prompt user for choice and store choice
+  let humanChoice = prompt("Please enter your choice");
+  return humanChoice;
+}
+  
+console.log(getComputerChoice());
+console.log(getHumanChoice());
+
+// End getComputerChoice function
+
+
+
 console.log("Hello, World!");
